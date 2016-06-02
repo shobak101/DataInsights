4bbb1a6e4d672ba33953e6d4167d9327c0cb2709
Edward Kim <edward.kim@concur.com>
2016-06-01 17:37:37 -0700

    updates

--- DataInsights.GmailCollector/main.py
+++ DataInsights.GmailCollector/main.py
@@ -4,17 +4,20 @@ import os
 import base64
 import email
 import pprint
+import re
 import pymssql
 from apiclient import discovery
 import oauth2client
 from oauth2client import client
 from oauth2client import tools
+from apiclient import errors
+#os.environ['TDSDUMP'] = 'stdout'
 
-SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
+SCOPES = 'https://mail.google.com/'
 CLIENT_SECRET_FILE = '/Users/edwardk/go/workspace/src/github.com/gosu517/datacollector/client_secret.json'
 APPLICATION_NAME = 'Gmail API Python Quickstart'
-#conn = pymssql.connect(server='streamanalyticsproject.database.windows.net', user='css553user', password='CSS553password', database='StreamAnalyticsProject')
-pp = pprint.PrettyPrinter(indent=2)
+conn = pymssql.connect(server="streamanalyticsproject.database.windows.net", port=1433, user="css553user@streamanalyticsproject.database.windows.net", password="CSS553password", database="StreamAnalyticsProject")
+
 def get_credentials():
     """Gets valid user credentials from storage.
 
@@ -45,11 +48,8 @@ def get_credentials():
 
 
 def main():
-    """Shows basic usage of the Gmail API.
 
-    Creates a Gmail API service object and outputs a list of label names
-    of the user's Gmail account.
-    """
+
     credentials = get_credentials()
     http = credentials.authorize(httplib2.Http())
     service = discovery.build('gmail', 'v1', http=http)
@@ -62,20 +62,70 @@ def main():
         print('No Mail')
     else:
         for message in messages:
-            print(message['id'])
-            mymessage = service.users().messages().get(userId='me', id=message['id'], format='full', metadataHeaders=None).execute()
-            payload = mymessage.get('payload', [])
-            parts = payload.get('parts', [])
-            pp.pprint(parts)
-            #msg_str = base64.urlsafe_b64decode(payload['headers'].encode('ASCII'))
-            #print(msg_str)
+            #print(message['id'])
+            mymessage = service.users().messages().get(userId='me', id=message['id'], format='raw', metadataHeaders=None).execute()
+            msg_str = base64.urlsafe_b64decode(mymessage['raw'].encode('ASCII'))
+
+            content = re.search('<div dir="ltr">(.+?)</div>', msg_str)
+            if content:
+              content=content.group(1)
+
+            author = re.search('From:(.+?)\n', msg_str)
+            if author:
+              author = author.group(1)
+
+            tstamp = re.search('Date:(.+?)\n', msg_str)
+            if tstamp:
+              tstamp = tstamp.group(1)
+
+            source='gmail'
+
+            #For demo purposes
+            print(author)
+            print(content)
+            print(tstamp+'\n')
+
+            #Label the message
+            service.users().messages().modify(userId='me', id=message['id'], body={"addLabelIds": ["TRASH"]}).execute()
+
+
             #---------------iterate through each mail 'id' and post to database then delete ore label
             #cursor = conn.cursor()
-            #cursor.execute("INSERT SalesLT.Product (Name, ProductNumber, StandardCost, ListPrice, SellStartDate) OUTPUT INSERTED.ProductID VALUES ('SQL Server Express', 'SQLEXPRESS', 0, 0, CURRENT_TIMESTAMP)")
+
+            #cursor.execute('SELECT TOP 1 * FROM RawDataTable)
             #row = cursor.fetchone()
             #while row:
-              #print "Inserted Product ID : " +str(row[0])
-              #row = cursor.fetchone()
+        #        print("Id=%d, Author=%s" % (row[0], row[1]))
+        #        row = cursor.fetchone()
+
+
+            #TESTS CONNECTION --------------------------
+            #cursor.execute("SELECT @@VERSION")
+            #print(cursor.fetchone()[0])
+
+
+            #cursor.executemany(
+            #"INSERT INTO RawDataTable VALUES ('source', 'author', 'timestamp', 'content')",
+            #[("gmail", "Edward Kim", "2016-05-16 01:53:19:000", "test message"),
+            # ("gmail", "Jane Doe", "2016-05-16 01:53:19:000", "foobar")
+            # ])
+
+            #cursor.execute(
+            #"INSERT INTO RawDataTable VALUES ('source', 'author', 'timestamp', 'content')",
+            #("gmail", author, tstamp, content)
+            #)
+
+            #cursor.execute('SELECT * FROM RawDataTable')
+            #print(cursor.fetchone())
+
+            #cursor.execute("INSERT StreamAnalyticsProject.RawDataTable ('source', 'author', 'timestamp', 'content') OUTPUT INSERTED.RawDataTableId VALUES ('gmail','Edward Kim','12-14-2015','test message')")
+
+            #row = cursor.fetchone()
+            #while row:
+            #  print (row[0])
+            #  row = cursor.fetchone()
+
+            #conn.commit()
 
 
 #msg_str = base64.urlsafe_b64decode(message['raw'].encode('ASCII'))
@@ -91,7 +141,7 @@ def main():
 #      print('Labels:')
 #      for label in labels:
 #        print(label['name'])
-
+#conn.close()
 
 if __name__ == '__main__':
     main()
