59a4b9b5cad449fd00ae6398968974e91553083f
Shawn Quinn <shawnquinn861@gmail.com>
2016-06-05 19:20:08 -0700

    added initial sports data collection code

---
+++ DataInsights.SportsDataCollectionSampleCode/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebClientTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MLBDataCollector mlbCollector = new MLBDataCollector();
+            string scores = mlbCollector.GetBoxScores("06", "05", "2016", "g4eryffabm8ap4v3f5zw7jem");
+            mlbCollector.saveBoxScores(scores);
+        }
+    }
+}
